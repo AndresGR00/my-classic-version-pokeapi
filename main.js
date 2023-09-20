0cadbb44d4226ec2bfee1affa7ada936ca5ec552
AndresGR00 <angare97@gmail.com>
2023-09-20 19:33:52 +0200

    add function to the info buttons

--- main.js
+++ main.js
@@ -28,6 +28,123 @@ let currentPokemon;
 let previousPokemon;
 let nextPokemon;
 
+// Info Buttons
+
+const attachEventListenersToButtons = () => {
+  const aboutButtons = document.querySelectorAll(".pk-about");
+  const statusButtons = document.querySelectorAll(".pk-status");
+  const movesButtons = document.querySelectorAll(".pk-moves");
+
+  aboutButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const card = button.closest(".pk-current-card");
+      const pokemonId = currentPokemon.id;
+      displayAboutInfo(card, pokemonId);
+    });
+  });
+
+  statusButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const card = button.closest(".pk-current-card");
+      const pokemonId = currentPokemon.id;
+      displayStatusInfo(card, pokemonId);
+    });
+  });
+
+  movesButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const card = button.closest(".pk-current-card");
+      const pokemonId = currentPokemon.id;
+      displayMovesInfo(card, pokemonId);
+    });
+  });
+};
+
+const displayAboutInfo = async (card, pokemonId) => {
+  try {
+    // Hacer una solicitud a la API para obtener información adicional del Pokémon
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
+    const res = await fetch(apiUrl);
+    const response = await res.json();
+
+    // Extraer la información relevante del Pokémon desde la API
+    const height = response.height;
+    const weight = response.weight;
+
+    // Crear el contenido HTML interpolado
+    const aboutInfo = `
+      <p class="pk-height"> Height: <strong>${height} ft</strong></p>
+      <p class="pk-width"> Weight: <strong>${weight} lbs</strong></p>
+    `;
+
+    // Insertar el contenido en la tarjeta
+    card.querySelector(".pk-content-info").innerHTML = aboutInfo;
+  } catch (err) {
+    console.log(err);
+    alert("Hubo un error al obtener la información del Pokémon.");
+  }
+};
+
+const displayStatusInfo = async (card, pokemonId) => {
+  try {
+    // Hacer una solicitud a la API para obtener información adicional del Pokémon
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
+    const res = await fetch(apiUrl);
+    const response = await res.json();
+
+    // Crear el contenido HTML interpolado
+    const statusInfo = `
+    <div class="pk-status-info">
+      <div class="pk-status-even">
+      <p class="pk-height"> ${response.stats[0].stat.name} <strong>${response.stats[0].base_stat}</strong> 
+      <p class="pk-height"> ${response.stats[1].stat.name} <strong>${response.stats[1].base_stat}</strong>           
+      <p class="pk-height"> ${response.stats[2].stat.name} <strong>${response.stats[2].base_stat}</strong>
+      </div>
+      <div class="pk-status-odds">
+      <p class="pk-height"> ${response.stats[5].stat.name} <strong>${response.stats[5].base_stat}</strong>
+      <p class="pk-height"> ${response.stats[3].stat.name} <strong>${response.stats[3].base_stat}</strong>
+      <p class="pk-height"> ${response.stats[4].stat.name} <strong>${response.stats[4].base_stat}</strong>
+      
+      </div>
+    </div>`;
+
+    // Insertar el contenido en la tarjeta
+    card.querySelector(".pk-content-info").innerHTML = statusInfo;
+  } catch (err) {
+    console.log(err);
+    alert("Hubo un error al obtener la información del Pokémon.");
+  }
+};
+
+const displayMovesInfo = async (card, pokemonId) => {
+  try {
+    // Hacer una solicitud a la API para obtener información adicional del Pokémon
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
+    const res = await fetch(apiUrl);
+    const response = await res.json();
+
+    // Crear el contenido HTML interpolado
+    const movesInfo = `<div class="pk-moves-names pk-display-flex">
+    <div class="pk-moves-left">
+      <p class="pk-height"><strong>${response.moves[0].move.name}</strong></p>
+      <p class="pk-height"><strong>${response.moves[1].move.name}</strong></p>
+    </div>
+    <div class="pk-moves-right">
+      <p class="pk-height"><strong>${response.moves[2].move.name}</strong></p>
+      <p class="pk-height"><strong>${response.moves[3].move.name}</strong></p>
+    </div>
+  </div>`;
+
+    // Insertar el contenido en la tarjeta
+    card.querySelector(".pk-content-info").innerHTML = movesInfo;
+  } catch (err) {
+    console.log(err);
+    alert("Hubo un error al obtener la información del Pokémon.");
+  }
+};
+
+//End Info Buttons
+
 const searchButton = document.querySelector(".pk-submit");
 searchButton.addEventListener("click", () => {
   const searchTerm = document.querySelector(".pk-search-bar").value;
@@ -120,6 +237,9 @@ const renderNextPokemon = (pokemon) => {
     pokemonColor
   );
   nextCard.innerHTML = pokemonCardTemplate(pokemon);
+
+  // Agregar manejadores de eventos después de agregar el contenido
+  attachEventListenersToButtons();
 };
 
 const renderPreviousPokemon = (pokemon) => {
@@ -134,6 +254,9 @@ const renderPreviousPokemon = (pokemon) => {
     pokemonColor
   );
   previousCard.innerHTML = pokemonCardTemplate(pokemon);
+
+  // Agregar manejadores de eventos después de agregar el contenido
+  attachEventListenersToButtons();
 };
 
 const previousSpan = document.querySelector(".pk-previous-span");
@@ -202,6 +325,9 @@ const fetchFirstPokemon = async () => {
 
     const nextPokemonId = currentPokemon.id + 1;
     searchNextPokemon(nextPokemonId);
+
+    // Agregar manejadores de eventos después de agregar el contenido
+    attachEventListenersToButtons();
   } catch (err) {
     console.log(err);
   }
